243e74feeb6ba6fcc84e6bb027eb2805b4c2110f
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-11-24 10:17:55 +0400

    Rename app reducer file and overwrite reducer logic to slices

---
+++ src/app/app-slice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  themeMode: "light" as ThemeMode,
+}
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: create => ({
+    changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+      state.themeMode = action.payload.themeMode
+    })
+  })
+})
+
+export const { changeThemeModeAC } = appSlice.actions
+export const appReducer = appSlice.reducer
+
+export type ThemeMode = "dark" | "light"
